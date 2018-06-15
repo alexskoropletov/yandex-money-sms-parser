931f2c856e11f55b42fb1ab4121bd49e11ff25d7
Alex Skoropletov <alex@skoropletov.ru>
2018-06-15 19:18:20 +0300

    first working (i guess) implementation

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+echo "[<]" . PHP_EOL . PHP_EOL;
+if (isset($argv[1])) {
+    $patterns = [
+        'code'   => [
+            'regex'  => '/([0-9]{4}(?![0-9]))/',
+            'name'   => 'Код подтверждения',
+            'result' => false,
+        ],
+        'amount' => [
+            'regex'  => '/([0-9]{1,6}\,[0-9]{2}р)/',
+            'name'   => 'Сумма к оплате',
+            'result' => false,
+        ],
+        'wallet' => [
+            'regex'  => '/([0-9]{14,15})/',
+            'name'   => 'ID кошелька',
+            'result' => false,
+        ],
+    ];
+    if ($message = file_get_contents($argv[1])) {
+        echo sprintf("SMS сообщение: %s'%s'", PHP_EOL, $message) . PHP_EOL . PHP_EOL;
+        $message = str_replace([" ", "\n", "\t", "\r"], '', $message);
+        foreach ($patterns as $pattern_key => $pattern) {
+            preg_match($pattern['regex'], $message, $matches);
+            if ($pattern_key) {
+                if (isset($matches[0])) {
+                    echo sprintf('%s: %s', $pattern['name'], $matches[0]) . PHP_EOL;
+                } else {
+                    echo sprintf("[!] Ошибка: не удалось найти %s", $pattern['name']) . PHP_EOL;
+                }
+            }
+        }
+    } else {
+        echo "[!] Ошибка: не удалось прочитать файл с сообщением" . PHP_EOL;
+    }
+} else {
+    echo "[!] Ошибка: не задан путь к файлу с сообщением" . PHP_EOL;
+}
+echo PHP_EOL . "[>]" . PHP_EOL;
